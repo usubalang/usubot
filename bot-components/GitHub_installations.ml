50aac4026514303a2385474c430a67beb0266184
mattiasdrp <5543639+mattiasdrp@users.noreply.github.com>
2022-03-07 11:32:51 +0100

    debuggin in the rain

--- bot-components/GitHub_installations.ml
+++ bot-components/GitHub_installations.ml
@@ -7,6 +7,8 @@ let action_with_new_installation_token ~bot_info ~key ~owner ~repo action () =
   (* Installation tokens expire after one hour, we stop using them after 40 minutes *)
   GitHub_app.get_installation_token ~bot_info ~key ~owner ~repo >>= function
   | Ok (install_token, expiration_date) ->
+      Format.eprintf "Installation token create: %s expiring %f (time is %f)@."
+        install_token expiration_date (Unix.time ());
       let _ =
         Base.Hashtbl.add installation_tokens ~key:owner
           ~data:(install_token, expiration_date)
@@ -16,6 +18,7 @@ let action_with_new_installation_token ~bot_info ~key ~owner ~repo action () =
       in
       action ~bot_info
   | Error _ ->
+      Format.eprintf "Error creating an installation token@.";
       (* If we cannot retrieve an installation token for the repository
          repo owned by owner, we execute the action with the github access token. *)
       action ~bot_info
@@ -27,6 +30,9 @@ let action_as_github_app ~bot_info ~key ~owner ~repo action
       () =
   match Base.Hashtbl.find installation_tokens owner with
   | Some (install_token, expiration_date) ->
+      Format.eprintf
+        "Found an installation token: %s expiring %f (time is %f)@."
+        install_token expiration_date (Unix.time ());
       if Base.Float.(expiration_date < Unix.time ()) then (
         Base.Hashtbl.remove installation_tokens owner;
         action_with_new_installation_token ~bot_info ~key ~owner ~repo action ())
@@ -36,10 +42,16 @@ let action_as_github_app ~bot_info ~key ~owner ~repo action
         in
         action ~bot_info
   | None -> (
+      Format.eprintf "No installation token found@.";
       GitHub_app.get_installations ~bot_info ~key >>= function
       | Ok installs ->
+          Format.eprintf "Installs found: %a@.Owner: %s@."
+            Format.(pp_print_list (fun ppf s -> Format.fprintf ppf "%s" s))
+            installs owner;
           if ListLabels.exists installs ~f:(String.equal owner) then
             action_with_new_installation_token ~bot_info ~key ~owner ~repo
               action ()
           else action ~bot_info
-      | Error _ -> action ~bot_info)
+      | Error _ ->
+          Format.eprintf "Error@.";
+          action ~bot_info)
